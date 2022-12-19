5722f7935628d0246a073b6c42677d8cdb229da5
Anderson <tseng.andersonn@gmail.com>
2022-12-19 00:22:27 -0800

    email verification i

--- main.py
+++ main.py
@@ -3,26 +3,21 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 
-from RolePicker import RolePicker
+from RolePicker import RoleMenuButton
+from EmailManager import EmailManager
 
 guilds = [714354863349170187, 753037165050593300, 511924606651727895]
 bot = discord.Bot(owner_id=221780012372721664, debug_guilds=guilds)
 
-# Button for role menu
-class RoleMenuButton(discord.ui.View): 
-    def __init__(self):
-        super().__init__(timeout=None)
-        
-    @discord.ui.button(label="Choose Roles!", custom_id="RoleMenuButton", style=discord.ButtonStyle.success) 
-    async def button_callback(self, button, interaction:discord.Interaction):
-        await interaction.response.send_message(
-            content="Pick your roles:", 
-            ephemeral=True, 
-            delete_after=300, 
-            view=RolePicker(interaction)
-        )
+# ROLE MENU        
+@bot.slash_command(description="Creates a role menu.")
+async def rolemenu(ctx):
+    await ctx.respond(
+        "Self-Assignable Roles!", 
+        view=RoleMenuButton()
+    ) 
 
-# Button for getting the planning role
+# PLANNING ROLE
 class ShenaniganButton(discord.ui.View): 
     def __init__(self):
         super().__init__(timeout=None)
@@ -39,29 +34,96 @@ class ShenaniganButton(discord.ui.View):
             await interaction.user.remove_roles(r)
             await interaction.response.send_message(f"Took away your planning role!", ephemeral=True, delete_after=5)
 
-@bot.slash_command() # Create a slash command
-async def rolemenu(ctx):
-    await ctx.respond(
-        "Self-Assignable Roles!", 
-        view=RoleMenuButton()
-    ) 
-    
-@bot.slash_command() # Create a slash command
+@bot.slash_command(description="Creates an planning role button.") 
 async def shenanigans(ctx):
     await ctx.respond(
         "Would you like to help organize events or be involved in the club? Grab a planning role today and help us make this club better!", 
         view=ShenaniganButton()
     ) 
+    
+# EMAIL VERIFICATION
+
+class EmailModal(discord.ui.Modal):
+    def __init__(self):
+        super().__init__(title="Please enter your email.")
+        self.add_item(discord.ui.InputText(label="enter your @mylangara.ca email here"))
+
+    async def callback(self, interaction: discord.Interaction):
+        email = self.children[0].value
+        await email_manager.start_verification(email, interaction)
+    
+class VerificationCodeModal(discord.ui.Modal):
+    def __init__(self):
+        super().__init__(title="Please enter your verification code.")
+        self.add_item(discord.ui.InputText(label="enter your verification code"))
+
+    async def callback(self, interaction: discord.Interaction):
+        code = self.children[0].value
+        await email_manager.check_verification_code(code, interaction)
+
+
+class EmailButtons(discord.ui.View): 
+    def __init__(self):
+        super().__init__(timeout=None)
+        
+    @discord.ui.button(label="Verify your email", custom_id="EmailButton", style=discord.ButtonStyle.green) 
+    async def button_callback_1(self, button, interaction:discord.Interaction):
+        
+        if email_manager.role in interaction.user.roles:
+            await interaction.response.send_message("You are already verified!", ephemeral=True)
+            return
+        await interaction.response.send_modal(EmailModal())
+
+    @discord.ui.button(label="Enter your verification code", custom_id="VerificationButton", style=discord.ButtonStyle.gray) 
+    async def button_callback_2(self, button, interaction:discord.Interaction):
+        
+        if email_manager.role in interaction.user.roles:
+            await interaction.response.send_message("You are already verified!", ephemeral=True)
+            return
+        await interaction.response.send_modal(VerificationCodeModal())
+
+@bot.slash_command(description="Creates an email verification menu.")
+async def email_verification(ctx):
+    await ctx.respond(
+        "Please click here to verify your email.",
+        view=EmailButtons()
+    )
+
+@bot.slash_command(description="Clear email data for a user.")
+@discord.default_permissions(administrator=True)
+async def clear_email_data(interaction:discord.Interaction, user_id):
+    email_manager.clear_data(user_id)
+    await interaction.respond(f"Cleared email data for {user_id}.", ephemeral=True)
+
+
+# MISC.
+@bot.slash_command(description="For benevolent dictators only.")
+@discord.default_permissions(administrator=True)
+async def execute(interaction:discord.Interaction, message):
+    try:
+        exec(message)
+        await interaction.respond(f"Executed `{message}`", ephemeral=True)
+    except:
+        await interaction.respond("Execution failed.", ephemeral=True)
+    
 
 @bot.event
 async def on_ready():
     # make buttons persistent through bot restarts
     bot.add_view(RoleMenuButton())
     bot.add_view(ShenaniganButton())
+    bot.add_view(EmailButtons())
+    
+    # initialize email manager
+    email_manager.role = bot.get_guild(753037165050593300).get_role(1054275502719914075)
+    email_manager.logging = bot.get_guild(753037165050593300).get_channel(1054250548762791936)
         
     print(f"Logged in as {bot.user} (ID: {bot.user.id})")
     print("------")
 
+
+# no, this doesn't violate OOP at all, why do you ask?
+email_manager = EmailManager("sqlite3", "database.sqlite", os.getenv("MAKE_GMAIL_WEBHOOK"))
 
 print("Running bot!")
 bot.run(os.getenv("DISCORD_TOKEN"))
