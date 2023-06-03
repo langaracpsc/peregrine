b9e69b5229129f4218250bcf2bdb0143b1325c45
Anderson <tseng.andersonn@gmail.com>
2023-06-02 23:11:05 -0700

    implement ephemeral channel

---
+++ python/cogs/Ephemeral.py
@@ -0,0 +1,89 @@
+import time
+import asyncio
+
+import discord
+from discord.ext import commands
+from better_profanity import profanity
+
+class Ephemeral(commands.Cog):
+    def __init__(self, bot:commands.Bot, ephemeral_channel=1114372341288402944):
+        self.bot = bot
+        self.message_ids: list[int] = []
+        
+        self.channel = ephemeral_channel
+        self.rules_message = 1114433687564587038
+        
+        self.delete_after = 60 * 60 * 24 * 2 # 48 hours
+        self.check_interval = 60*60 # 1 hour
+        
+    async def async_init(self):
+        
+        channel = await self.bot.fetch_channel(self.channel)
+        assert(channel != None)
+        assert(channel.name == "ephemeral")
+        asyncio.create_task(self.delete_old_messages())
+    
+    # Repeats any messages sent in #ephemeral
+    @commands.Cog.listener()
+    @discord.default_permissions(administrator=True)
+    async def on_message(self, message:discord.Message):
+        if message.author.id == self.bot.application_id:
+            return
+        
+        if message.channel.id != self.channel:
+            return
+        
+        if message.content.strip() == "":
+            return
+        
+        await message.delete()
+        
+        new_text = profanity.censor(message.content)
+        
+        embed = discord.Embed(
+            title=f"Ephemeral message:",
+            description=f"{new_text}",
+        )
+        embed.set_footer(text="This message will disappear in 48 hours.")
+
+        message = await message.channel.send(embed=embed)
+                
+    # this code seems mildly dangerous i hope it doesn't explode
+    async def delete_old_messages(self):
+        
+        while True:
+            await asyncio.sleep(self.check_interval) # run this code every hour
+            
+            async for message in self.bot.get_channel(self.channel).history():
+                
+                # don't delete rules
+                if message.id == self.rules_message:
+                    continue
+                            
+                if ( time.time() - message.created_at.timestamp() ) > self.delete_after:
+                    
+                    await message.delete()    
+            
+            
+             
+                
+    @commands.slash_command(description="Sends rules for #ephemeral")
+    @discord.default_permissions(administrator=True)
+    async def send_ephemeral_rules(self, interaction:discord.Interaction):
+        
+        embed = discord.Embed(
+            title = "Rules:",
+            description = "- Messages are mirrored by <@896896783123357729>.\n- Messages in this channel delete themselves after 48 hours.\n- Message modmail if you need to delete your message.\n- Server rules still apply.",
+        )
+        embed.set_footer(text="Like its contents, this channel may disappear in the future.")
+
+        if self.rules_message == None:
+            await self.bot.get_channel(self.channel).send(embed=embed)
+        else:
+            await self.bot.get_channel(self.channel).fetch_message(self.rules_message)
+            await self.bot.get_message(self.rules_message).edit(embed=embed)
+            await interaction.respond("Done.", ephemeral=True)
+            
+            
+def setup(bot:commands.Bot):
+    bot.add_cog(Ephemeral(bot))
--- python/main.py
+++ python/main.py
@@ -5,45 +5,72 @@ import time
 from dotenv import load_dotenv
 from os import environ
 
-# Test token exists
+# Test that token exists.
+
 load_dotenv()
 if "DISCORD_BOT_TOKEN" not in environ:
     raise Exception("Please provide a discord bot token.")
 
 # Initialize bot.
+
 owner_ids:tuple[int] = (
     221780012372721664, # Highfire1#1942
     461139809583366154, # coderis.h#1684
 )
 
-bot = commands.Bot(owner_ids=owner_ids, command_prefix=commands.when_mentioned_or("?"))
+intents = discord.Intents.default()
+intents.message_content = True
 
+bot = commands.Bot(owner_ids=owner_ids, command_prefix=commands.when_mentioned_or("?"), intents=intents)
 
 # Import extensions.
 
 extensions = (
     "cogs.Admin", 
-    "cogs.CourseInfo"
+    #"cogs.CourseInfo",
+    "cogs.Anonymous",
     )
 bot.load_extensions(*extensions)
 
+# misc.
 @bot.event
 async def on_ready():
+    await async_init()
+    
     print(f"Logged in as {bot.user}! (ID: {bot.user.id})\n")
     
+# Command to reload extensions.
+
 @bot.slash_command(name="reload_extensions")    
 @discord.default_permissions(administrator=True) 
-async def reload_extensions(ctx: discord.ApplicationContext):
+async def reload_extensions(ctx: discord.ApplicationContext, specific_cog:str = None):
     start = time.time()
     reply = await ctx.respond(content="Reloading extensions...", ephemeral=True)
     
-    for e in extensions:
-        bot.unload_extension(e)
-    bot.load_extensions(*extensions)
+    reload = extensions
+    if specific_cog != None:
+        reload = (specific_cog)
+    
+    reply_msg = ""
+    
+    for e in reload:
+        try:
+            bot.reload_extension(e)
+        except Exception as e:
+            reply_msg += str(e) + "\n\n"
+            
+    await async_init()
     
     end = str(time.time() - start)[:4] 
-    await reply.edit_original_response(content=f"Extensions reloaded in {end} seconds.")
+    reply_msg += f"Extensions reloaded in {end} seconds."
+    await reply.edit_original_response(content=reply_msg)
         
-    
+# Launch bot.
+
 bot.run(environ.get("DISCORD_BOT_TOKEN"))
 
+
+# command to initialize async methods in cogs.
+
+async def async_init(bot):
+    await bot.get_cog("Ephemeral").async_init()
