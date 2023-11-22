528b3280cde0bb1e61f52bda05f5e2b29ff7aa00
Anderson <tseng.andersonn@gmail.com>
2023-11-21 22:45:47 -0800

    RCE is bad and fix the watchtower integration

--- python/cogs/Admin.py
+++ python/cogs/Admin.py
@@ -6,16 +6,6 @@ import requests
 class Admin(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-    
-    # Executes code on the server
-    @commands.slash_command(description="For administrative purposes only.")
-    @discord.default_permissions(administrator=True)
-    async def execute(self, interaction:discord.Interaction, message):
-        try:
-            exec(message)
-            await interaction.respond(f"Executed `{message}`", ephemeral=True)
-        except:
-            await interaction.respond("Execution failed.", ephemeral=True)
 
     # Asks watchtower to check for updates
     @commands.slash_command(description="Updates docker containers if an update exists")
@@ -30,7 +20,8 @@ class Admin(commands.Cog):
                 # go in this order so that the message actually makes it to discord
                 requests.post(url)
                 await interaction.respond("Update request sent to the cloud.")
-                await requests.post(url, headers=headers)
+                requests.post(url, headers=headers)
+                
             except Exception as e:
                 await interaction.respond("Could not establish a connection: " + str(e))
 
