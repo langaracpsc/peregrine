a037f7ec41683bf5a0af6bda2c558cc2d4d912af
Anderson <tseng.andersonn@gmail.com>
2024-01-23 08:52:11 -0800

    Improve automoderation system

--- python/cogs/AntiSpam.py
+++ python/cogs/AntiSpam.py
@@ -3,6 +3,7 @@ import asyncio
 
 import discord
 from discord.ext import commands
+from discord.utils import get
 from better_profanity import profanity
 
 '''
@@ -21,7 +22,7 @@ class AntiSpam(commands.Cog):
         self.moderation_channel = 1040768757037023292
         self.moderator_role = 753074601084583997
         
-        self.stored_messages = 20
+        self.stored_messages = 100
         self.trigger_warning = 5
         
         self.must_have_url = False
@@ -31,7 +32,7 @@ class AntiSpam(commands.Cog):
     # Listens to all messages
     @commands.Cog.listener()
     @discord.default_permissions(administrator=True)
-    async def on_message(self, message:discord.Message):
+    async def on_message(self, message:discord.Message):        
         
         # ignore messages sent by bot
         if message.author.id == self.bot.application_id:
@@ -42,12 +43,12 @@ class AntiSpam(commands.Cog):
             return
         
         # don't alert unless message has a url and option enabled
-        if not self.must_have_url and not 'https://' in message.content:
-            return
+        # if not self.must_have_url and not 'https://' in message.content:
+        #     return
         
         # don't alert if a moderator sends spam
-        if message.author.guild_permissions.manage_messages:
-            return
+        # if message.author.guild_permissions.manage_messages:
+        #     return
         
         msg = (message.author.id, hash(self.hash_salt + message.content))
         self.recent_messages.append(msg)
@@ -58,7 +59,7 @@ class AntiSpam(commands.Cog):
         
         # prevent memory leaks
         # (although this should NEVER be run because it means that we have had 100 scam alerts)
-        if len(self.warned_messages) > 100:
+        if len(self.warned_messages) > 200:
             self.warned_messages.pop(0)        
         
         # look for duplicated messages
@@ -77,10 +78,13 @@ class AntiSpam(commands.Cog):
                     title=f"Potential Spam Detected",
                     description=f"{m[1]}\n{message.jump_url}\n\nThis message was sent `{count}` times by <@{m[0]}>.",
                 )
-                embed.set_footer(text="If this isn't spam, you can safely ignore this message.")
+                embed.set_footer(text="If this isn't spam, then you need to remove the quarantined role from the above user.")
 
                 await self.bot.get_channel(self.moderation_channel).send(content=f"<@&{self.moderator_role}> potential spam detected!",allowed_mentions=discord.AllowedMentions(roles=True))
                 await self.bot.get_channel(self.moderation_channel).send(embed=embed)
+                
+                quarantine_role = message.guild.get_role(1041191067019657267)
+                await message.author.add_roles(quarantine_role)
                 
                 
 def setup(bot:commands.Bot):
